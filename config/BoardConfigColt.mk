611b43ed4125338b7b7392e1fda491134f1cb4ad
Michael Bestas <mkbestas@lineageos.org>
2018-03-21 18:29:45 +0200

    liquid: Set TARGET_CRYPTFS_HW_PATH if HW crypto is enabled
    * So TWRP can use it
    
    Change-Id: Ibc1d9d943d6d4e4e5248f623cd06782335f8ae98

--- config/BoardConfigColt.mk
+++ config/BoardConfigColt.mk
@@ -5,3 +5,8 @@ include vendor/colt/config/BoardConfigQcom.mk
 endif
 
 include vendor/colt/config/BoardConfigSoong.mk
+
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+endif
