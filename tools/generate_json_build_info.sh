a1458cecf42013c31ab1ea04b29347db78955c96
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-22 16:13:10 -0300

    [Squashed] vendor: build: Generate json file with zip info after build [1/2]
    From 773a212548781b793afae4eb80f7bf893193969d Mon Sep 17 00:00:00 2001
    From: abhinavgupta371 <abhinavgupta371@gmail.com>
    Date: Wed, 27 Mar 2019 20:22:24 +0530
    Subject: [PATCH] vendor: tools: Generate OTA variables
    
    From 2b489f72b3f53433056bae875b95758b0848fed1 Mon Sep 17 00:00:00 2001
    From: Akito Mizukito <peak@echln.net>
    Date: Fri, 9 Aug 2019 22:10:41 +0900
    Subject: [PATCH] vendor: tools: Make the generator suit the current OTA system needs
    
    From 2da68a1abd1bb258b01bc52a1533ecb25c04c2fc Mon Sep 17 00:00:00 2001
    From: =?UTF-8?q?=E3=81=82=E3=81=8D=E3=81=A8=20=E3=83=9F=E3=82=BA=E3=82=AD?=
     =?UTF-8?q?=E3=83=88?= <peak@echln.net>
    Date: Tue, 8 Oct 2019 02:15:57 +0900
    Subject: [PATCH] Fixed JSON file_size parsing
    
    From 13a94e03d9ad220b6c344870c09d66acaccc6970 Mon Sep 17 00:00:00 2001
    From: Stallix <team.evolutionx.rom@gmail.com>
    Date: Sat, 7 Dec 2019 07:30:54 +0000
    Subject: [PATCH] vendor: tools: Make the generator follow the OTA format
    
    From 1da63c5174ac98c1f63d07f070ff3ecf88c2dbf3 Mon Sep 17 00:00:00 2001
    From: Joey Huab <joey@evolution-x.org>
    Date: Sun, 8 Mar 2020 20:33:16 +0000
    Subject: [PATCH] vendor: tools: Sort the json to suit the current OTA system
    
    From 041d90d8dfd561fe0b8a93a17c7a8708321b459c Mon Sep 17 00:00:00 2001
    From: SahilSonar <sss.sonar2003@gmail.com>
    Date: Wed, 21 Oct 2020 06:11:40 +0900
    Subject: [PATCH] tools: fix changelog generation
    
    From 07cc7fb9c980749c543c97d90093407853d99d5e Mon Sep 17 00:00:00 2001
    From: Justin Crawford <Justin@stacksmash.net>
    Date: Thu, 4 Feb 2021 17:34:03 +0530
    Subject: [PATCH] vendor: tools: Fix json generation for codenames with underscores
    
    Co-authored-by: abhinavgupta371 <abhinavgupta371@gmail.com>
    Co-authored-by: Akito Mizukito <peak@echln.net>
    Co-authored-by: Stallix <team.evolutionx.rom@gmail.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: SahilSonar <sss.sonar2003@gmail.com>
    Co-authored-by: Justin Crawford <Justin@stacksmash.net>

---
+++ tools/generate_json_build_info.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ "$1" ]
+then
+  file_path=$1
+  file_name=$(basename "$file_path")
+  DEVICE=$(echo $TARGET_PRODUCT | sed 's/colt_//g')
+  if [ -f $file_path ]; then
+    file_size=$(stat -c%s $file_path)
+    md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
+    datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+    id=$(sha256sum $file_path | awk '{ print $1 }');
+    echo -e "{\n   \"datetime\": $datetime,\n   \"filehash\": \"$md5\",\n   \"filename\": \"$file_name\",\n   \"id\": \"$id\",\n   \"size\": $file_size\n}" > $file_path.json
+  fi
+fi
