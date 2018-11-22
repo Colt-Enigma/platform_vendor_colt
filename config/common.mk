BootleggersROM/vendor_bootleggers

[2/2] Add 'GoBold' font family
Required for lockscreen. Also, added in case of adding more fonts.

based on this commit, credits to beanstown and jhenrique
https://gitlab.com/PixelExperience/vendor_pixelstyle/commit/faa3d8365bc1fa0d885e3e98f1e609f6e0fb85fb

config/common.mk
@@ -123,6 +123,16 @@ PRODUCT_PACKAGES += \
     OmniJaws \
     OmniStyle
 
+# Bootleggers Stuff - Copy to System fonts
+PRODUCT_COPY_FILES += \
+    vendor/bootleggers/prebuilt/fonts/gobold/Gobold.ttf:system/fonts/Gobold.ttf \
+    vendor/bootleggers/prebuilt/fonts/gobold/Gobold-Italic.ttf:system/fonts/Gobold-Italic.ttf \
+    vendor/bootleggers/prebuilt/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
+    vendor/bootleggers/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
+    vendor/bootleggers/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
+    vendor/bootleggers/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf
+
+
 # MusicFX advanced effects
 #ifneq ($(TARGET_NO_DSPMANAGER), true)
 #PRODUCT_PACKAGES += \

Colt-Enigma/platform_vendor_colt

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

config/common.mk
@@ -161,7 +161,16 @@ PRODUCT_PACKAGES += \
     rsync \
     zip
 
-# Exchange support
+# Colt Fonts - Copy to System fonts
+PRODUCT_COPY_FILES += \
+    vendor/colt/prebuilt/fonts/gobold/Gobold.ttf:system/fonts/Gobold.ttf \
+    vendor/colt/prebuilt/fonts/gobold/Gobold-Italic.ttf:system/fonts/Gobold-Italic.ttf \
+    vendor/colt/prebuilt/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
+    vendor/colt/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
+    vendor/colt/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
+    vendor/colt/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf
+
+# Exchange Support
 PRODUCT_PACKAGES += \
     Exchange2
 