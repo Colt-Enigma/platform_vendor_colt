6c2929457c3bcd29113089b268aa9c3af81c3173
ElDainosor <eldainosor@gmail.com>
2019-06-09 19:09:32 -0300

    Add 5 more styles for QS [3/3]
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- config/common.mk
+++ config/common.mk
@@ -134,7 +134,12 @@ PRODUCT_PACKAGES += \
     QStileBadge \
     QStileBadgetwo \
     QStileSquircle \
-    QStileDiamond
+    QStileDiamond \
+    QStileNeonlike \
+    QStileOOS \
+    QStileTriangles \
+    QStileDivided \
+    QStileCosmos
 
 # Omni's PoorMan Themes
 PRODUCT_PACKAGES += \
