BeastRoms/platform_vendor_beast

vendor: google: Add some google apps
* Markup
* SoundPicker
* Wellbeing

Signed-off-by: mdeejay <mdjrussia@gmail.com>

config/common.mk
@@ -27,7 +27,7 @@ endif
 
 # Bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/beast/prebuilt/common/media/bootanimation.zip:system/product/media/bootanimation.zip \
+    vendor/beast/prebuilt/common/media/bootanimation.zip:system/product/media/bootanimation.zip
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
     mkfs.exfat
 endif
 
+# Markup libs
+PRODUCT_COPY_FILES += \
+    vendor/beast/prebuilt/google/lib/libsketchology_native.so:system/product/lib/libsketchology_native.so \
+    vendor/beast/prebuilt/google/lib64/libsketchology_native.so:system/product/lib64/libsketchology_native.so
 
 # We modify several neverallows, so let the build proceed
 ifneq ($(TARGET_BUILD_VARIANT),user)

Colt-Enigma/platform_vendor_colt

Change-Id: I73ddba26c4556244730a0e4f3e80e370fc323401
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

config/common.mk
@@ -56,6 +56,11 @@ $(foreach f,$(wildcard vendor/colt/prebuilt/common/etc/init/*.rc),\
 PRODUCT_COPY_FILES += \
     vendor/colt/prebuilt/common/lib/content-types.properties:$(TARGET_COPY_OUT_SYSTEM)/lib/content-types.properties
 
+# Markup libs
+PRODUCT_COPY_FILES += \
+    vendor/colt/prebuilt/google/lib/libsketchology_native.so:system/product/lib/libsketchology_native.so \
+    vendor/colt/prebuilt/google/lib64/libsketchology_native.so:system/product/lib64/libsketchology_native.so
+
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
     vendor/colt/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
@@ -78,7 +83,9 @@ PRODUCT_COPY_FILES += \
 
 # Colt packages
 PRODUCT_PACKAGES += \ 
-    GalleryGoPrebuilt
+    GalleryGoPrebuilt \
+    MarkupGoogle \
+    SoundPickerPrebuilt
 
 # Power whitelist
 PRODUCT_COPY_FILES += \