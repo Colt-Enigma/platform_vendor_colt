nitrogen-project/android_vendor_nitrogen

prebuilt: Add a script to simplify A-only recovery system  mount
* This can be helpful because of the fragmentation that will happen with the system mount in recovery after Q

products/common.mk
@@ -93,7 +93,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/nitrogen/prebuilt/common/addon.d/50-nitrogen.sh:system/addon.d/50-nitrogen.sh \
     vendor/nitrogen/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/nitrogen/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
+    vendor/nitrogen/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/nitrogen/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
 
 # Priv-app config
 PRODUCT_COPY_FILES += \

Colt-Enigma/platform_vendor_colt

config/common.mk
@@ -31,7 +31,8 @@ PRODUCT_COPY_FILES += \
     vendor/colt/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/colt/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/colt/prebuilt/common/bin/50-colt.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-colt.sh \
-    vendor/colt/prebuilt/common/bin/blacklist:$(TARGET_COPY_OUT_SYSTEM)/addon.d/blacklist
+    vendor/colt/prebuilt/common/bin/blacklist:$(TARGET_COPY_OUT_SYSTEM)/addon.d/blacklist \
+    vendor/colt/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \