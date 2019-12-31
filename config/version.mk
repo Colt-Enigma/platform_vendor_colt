d927a598fb3e04e79b3d79d9a7912533bac2445f
RakeshBatra <raakesh.batra@rediffmail.com>
2019-12-31 13:37:46 +0000

    Welcome Colt v5.5 :)
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- config/version.mk
+++ config/version.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2018 ColtOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Versioning System
 
 ifndef COLT_BUILD_TYPE
@@ -13,7 +27,11 @@ endif
 TARGET_PRODUCT_SHORT := $(subst colt_,,$(TARGET_PRODUCT))
 
 # Set all versions
-COLT_VERSION = 5.4
+COLT_VERSION = 5.5
 COLT_BUILD_DATE := $(shell date -u +%d-%m-%Y)
-COLT_BUILD_VERSION := ColtOS-v$(COLT_VERSION)-$(shell date -u +%Y%m%d)-$(COLT_BUILD_TYPE)-$(TARGET_PRODUCT_SHORT)
+COLT_BUILD_VERSION := ColtOS-v$(COLT_VERSION)-$(COLT_BUILD_TYPE)-$(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)
 COLT_FINGERPRINT := ColtOS/v$(COLT_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d)/$(shell date -u +%H%M)
+COLT_DISPLAY_VERSION := ColtOS-v$(COLT_VERSION)-$(COLT_BUILD_TYPE)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.colt.display.version=$(COLT_DISPLAY_VERSION)
