Project-Xtended/vendor_xtended

Themes: Add DU themes [2/3]

config/common.mk
@@ -287,6 +287,9 @@ DEVICE_PACKAGE_OVERLAYS += vendor/xtended/overlay/common
 -include vendor/xtended/config/version.mk
 -include vendor/xtended/config/partner_gms.mk
 
+# Include Xtended theme files
+include vendor/xtended/themes/backgrounds/themes.mk
+
 # Fonts
 PRODUCT_COPY_FILES += \
     vendor/xtended/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \

Colt-Enigma/platform_vendor_colt

config/common.mk
@@ -271,6 +271,9 @@ PRODUCT_PACKAGES += \
     OmniJaws \
     WeatherIcons
 
+# Include Colt's theme files
+include vendor/colt/themes/backgrounds/themes.mk
+
 # Colt Stuff - Copy to System fonts
 PRODUCT_COPY_FILES += \
     vendor/colt/prebuilt/fonts/gobold/Gobold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Gobold.ttf \