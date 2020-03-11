26d1c1f76d25beea81c76597149757b1bfb6837d
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-03-11 12:24:26 +0100

    envsetup: s/wait-for-online/wait-for-device-recovery/g
    Change-Id: I3fd52aa99d281837fdbdba3b1d268572061a900d

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -97,7 +97,7 @@ function eat()
             return 1
         fi
         echo "Waiting for device..."
-        adb wait-for-online
+        adb wait-for-device-recovery
         echo "Found device"
         if (adb shell getprop ro.lineage.device | grep -q "$LINEAGE_BUILD"); then
             echo "Rebooting to sideload for install"
@@ -322,9 +322,9 @@ function installboot()
             return 1
         fi
     fi
-    adb wait-for-online
+    adb wait-for-device-recovery
     adb root
-    adb wait-for-online
+    adb wait-for-device-recovery
     if (adb shell getprop ro.lineage.device | grep -q "$LINEAGE_BUILD");
     then
         adb push $OUT/boot.img /cache/
@@ -360,9 +360,9 @@ function installrecovery()
             return 1
         fi
     fi
-    adb wait-for-online
+    adb wait-for-device-recovery
     adb root
-    adb wait-for-online
+    adb wait-for-device-recovery
     if (adb shell getprop ro.lineage.device | grep -q "$LINEAGE_BUILD");
     then
         adb push $OUT/recovery.img /cache/
@@ -473,7 +473,6 @@ function dopush()
         adb connect "$TCPIPPORT"
     fi
     adb wait-for-device &> /dev/null
-    sleep 0.3
     adb remount &> /dev/null
 
     mkdir -p $OUT
