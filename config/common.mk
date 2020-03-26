91629694f56610e1d1fa47ab80a2288c044f8937
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-03-26 08:15:14 +0100

    Allow A/B OTA downgrades on debuggable builds
    Change-Id: I361c6d71f7b4316cf023ad0b36fdc488e37d6176

--- config/common.mk
+++ config/common.mk
@@ -42,6 +42,10 @@ PRODUCT_COPY_FILES += \
     vendor/colt/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
     vendor/colt/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
     vendor/colt/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.ota.allow_downgrade=true
+endif
 endif
 
 # Backup Services whitelist
