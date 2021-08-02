71b9a34c7247d29a7f394d847261d2e8d93403c4
LuK1337 <priv.luk@gmail.com>
2021-08-02 16:48:10 -0400

    vendor: Don't enforce inclusion of common compatibility matrix
    * Let people include it if they choose to.
    * Avoids issues where people who previously didn't need
      compatibility matrix's are hitting checkvintf errors.
    
    * Fixes breakage in: 78870c267fa57b3cb98607c0715504e6821fd7b6
    
    Change-Id: I33bc1e67e7f9eb9a01930113535800a8e4f539fd
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- config/common.mk
+++ config/common.mk
@@ -274,8 +274,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/colt/config/device_framework_matrix.xml
-
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/colt/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/colt/overlay/common
 
