awaken-lemonkebab/android_vendor_awaken

vendor: overlay: Update from SP1A.210812.016
Change-Id: Idd547f7c5d0a2cf4509d3254346f854f272232ea
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

config/gapps.mk
@@ -1,0 +1,14 @@
+# Gapps
+ifeq ($(USE_GAPPS),true)
+$(call inherit-product, vendor/gms/gms_full.mk)
+
+# Common Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/awaken/overlay-gapps/common
+
+# Exclude RRO Enforcement
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+    vendor/awaken/overlay-gapps
+
+$(call inherit-product, vendor/awaken/config/rro_overlays.mk)
+endif

Colt-Enigma/platform_vendor_colt

Change-Id: I1887ca521101d57d67621bcb43d70e4b7590fa1a

config/gapps.mk
@@ -1,0 +1,14 @@
+# Gapps
+ifeq ($(WITH_GAPPS),true)
+$(call inherit-product, vendor/gms/gms_full.mk)
+
+# Common Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/colt/overlay-gapps/common
+
+# Exclude RRO Enforcement
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+    vendor/colt/overlay-gapps
+
+$(call inherit-product, vendor/colt/config/rro_overlays.mk)
+endif