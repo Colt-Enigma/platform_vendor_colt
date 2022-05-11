20697162a4aaec7b336081601aed68703c20e4ba
Jiakai Zhang <jiakaiz@google.com>
2022-05-11 16:54:01 +0100

    config: Add a system property for System UI compiler filter.
    Bug: 227310505
    Test: m
    Change-Id: I30863fe677a95844f0cf46da98c103c0132c24d3
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- config/common.mk
+++ config/common.mk
@@ -26,6 +26,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.usb.config=none
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
+# SystemUI
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    dalvik.vm.systemuicompilerfilter=speed
+
 # Spoof fingerprint for Google Play Services and SafetyNet
 ifeq ($(PRODUCT_OVERRIDE_GMS_FINGERPRINT),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
