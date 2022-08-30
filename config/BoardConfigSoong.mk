65a2ef87443ac19ec679ab66c8a8800e4754aa27
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 03:13:55 +0300

    config: Remove TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE
    This hasn't been ported after lineage-17.1
    
    Change-Id: I2aff9bb7bc17fa1444a904d47248d885268bf29b
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -34,7 +34,6 @@ SOONG_CONFIG_coltGlobalVars += \
     disable_postrender_cleanup \
     has_hh_vsync_issue \
     has_memfd_backport \
-    ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
     camera_needs_client_info \
     target_init_vendor_lib \
@@ -65,7 +64,6 @@ endif
 SOONG_CONFIG_coltGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_coltGlobalVars_has_hh_vsync_issue := $(TARGET_HAS_HH_VSYNC_ISSUE)
 SOONG_CONFIG_coltGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
-SOONG_CONFIG_coltGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_coltGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_coltNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_coltQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
