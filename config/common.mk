5d46930bed3b8a4dfbda2c3d87e4e99c6b495394
RakeshBatra <raakesh.batra@rediffmail.com>
2022-10-23 06:30:50 -0700

    Call Vendor/addon

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,6 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product-if-exists, vendor/addons/config.mk)
 
 PRODUCT_BRAND ?= ColtOS
 
