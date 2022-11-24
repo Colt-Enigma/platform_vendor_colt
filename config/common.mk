42b4136f83a5d4431a7174902a6478941088ff96
jhenrique09 <jhsv09@gmail.com>
2022-11-25 00:38:07 +0530

    vendor: Add support for face unlock
    Change-Id: Ib34a62f3cfd573e116f183793b24c7df41eb72f0
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- config/common.mk
+++ config/common.mk
@@ -68,6 +68,17 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/colt/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
