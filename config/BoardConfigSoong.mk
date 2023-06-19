Project-Xtended/vendor_xtended

vendor: soong: Add camera_needs_client_info_lib
Signed-off-by: firebird11 <hbgassel@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

config/BoardConfigSoong.mk
@@ -32,6 +32,7 @@ SOONG_CONFIG_xtendedGlobalVars += \
     disable_bluetooth_le_read_buffer_size_v2 \
     disable_bluetooth_le_set_host_feature \
     gralloc_handle_has_reserved_size \
+    camera_needs_client_info_lib \
     needs_camera_boottime \
     target_init_vendor_lib \
     target_ld_shim_libs \
@@ -57,6 +58,7 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_xtendedGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_xtendedGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_xtendedGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_xtendedGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_xtendedGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)

Colt-Enigma/platform_vendor_colt

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

config/BoardConfigSoong.mk
@@ -36,6 +36,7 @@ SOONG_CONFIG_coltGlobalVars += \
     disable_bluetooth_le_read_buffer_size_v2 \
     disable_bluetooth_le_set_host_feature \
     gralloc_handle_has_reserved_size \
+    camera_needs_client_info_lib \
     needs_camera_boottime \
     target_init_vendor_lib \
     target_inputdispatcher_skip_event_key \
@@ -75,6 +76,7 @@ SOONG_CONFIG_coltGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_
 SOONG_CONFIG_coltGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_coltGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_coltGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_coltGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0