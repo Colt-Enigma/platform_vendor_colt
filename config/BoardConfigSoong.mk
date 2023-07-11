78944ba0fd32f7bd376d02e16ef9775596df6c95
jhenrique09 <jhsv09@gmail.com>
2023-07-11 20:27:13 -0300

    vendor: Add TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT
    Change-Id: I0b5ded178ec35f87f768509e497ecce4926d4336
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -33,6 +33,7 @@ SOONG_CONFIG_coltGlobalVars += \
     has_hh_vsync_issue \
     has_memfd_backport \
     camera_needs_client_info \
+    camera_uses_newer_hidl_override_format \
     disable_bluetooth_le_read_buffer_size_v2 \
     disable_bluetooth_le_set_host_feature \
     gralloc_handle_has_reserved_size \
@@ -79,6 +80,7 @@ SOONG_CONFIG_coltGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_coltGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_coltGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_coltGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
+SOONG_CONFIG_palladiumGlobalVars_camera_uses_newer_hidl_override_format := $(TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
