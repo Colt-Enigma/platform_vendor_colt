fc9a586b07efc8b6357b081f009273ebe0f947d9
RakeshBatra <raakesh.batra@rediffmail.com>
2023-12-03 19:29:37 +0100

    Revert "soong: Add support to set vendor tag with client package name"
    This reverts commit 710442939c92a208ff15189910dc36648d25428a.
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -34,7 +34,7 @@ SOONG_CONFIG_coltGlobalVars += \
     has_memfd_backport \
     camera_needs_client_info \
     camera_override_format_from_reserved \
-    uses_nothing_camera \
+    camera_uses_newer_hidl_override_format \
     disable_bluetooth_le_read_buffer_size_v2 \
     disable_bluetooth_le_set_host_feature \
     gralloc_handle_has_reserved_size \
@@ -84,7 +84,7 @@ SOONG_CONFIG_coltGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEED
 SOONG_CONFIG_coltGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 SOONG_CONFIG_coltGlobalVars_target_alternative_futex_waiters := $(TARGET_ALTERNATIVE_FUTEX_WAITERS)
 SOONG_CONFIG_coltGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
-SOONG_CONFIG_coltGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_coltGlobalVars_camera_uses_newer_hidl_override_format := $(TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
